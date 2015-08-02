012dad5e4ce03d8900974fc3252f57f039a7874d
Andrey Antukh <niwi@niwi.nz>
2015-08-02 10:23:09 +0300

    Minor cosmetic changes on promesa.core-tests ns.

--- test/promesa/core_tests.cljs
+++ test/promesa/core_tests.cljs
@@ -117,9 +117,8 @@
 
 (set! *main-cli-fn* #(t/run-tests))
 
-
-(defmethod cljs.test/report [:cljs.test/default :end-run-tests]
+(defmethod t/report [:cljs.test/default :end-run-tests]
   [m]
-  (if (cljs.test/successful? m)
+  (if (t/successful? m)
     (set! (.-exitCode js/process) 0)
     (set! (.-exitCode js/process) 1)))
