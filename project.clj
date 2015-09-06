0521ce7e98113988eac05489a9c197822af3f3fe
Andrey Antukh <niwi@niwi.nz>
2015-09-06 19:55:25 +0300

    Set version to 0.5.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject funcool/promesa "0.4.0"
+(defproject funcool/promesa "0.5.0-SNAPSHOT"
   :description "A promise library for ClojureScript"
   :url "https://github.com/funcool/promesa"
   :license {:name "BSD (2 Clause)"
