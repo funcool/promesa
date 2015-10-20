6fc2a3491ef4f6365734f09df09e109e0f0e16b5
Alejandro Gómez <alejandro@dialelo.com>
2015-10-20 15:36:39 +0200

    Use node REPL instead of Nashorn
    Nashorn doesn't provide a suitable async scheduler for Bluebird out of
    the box and Node does.
    
    See https://github.com/petkaantonov/bluebird/wiki/Error:-No-async-scheduler-available

--- scripts/repl.clj
+++ scripts/repl.clj
@@ -1,8 +1,8 @@
 (require
   '[cljs.repl :as repl]
-  '[cljs.repl.nashorn :as nashorn])
+  '[cljs.repl.node :as node])
 
 (cljs.repl/repl
- (cljs.repl.nashorn/repl-env)
+ (node/repl-env)
  :output-dir "out"
  :cache-analysis true)
