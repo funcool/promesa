62b4ec2a58530791b4edce4010d663302da96dd4
Andrey Antukh <niwi@niwi.nz>
2015-12-02 18:45:21 +0200

    Update cats to 1.2.0.

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://opensource.org/licenses/BSD-2-Clause"}
   :dependencies [[org.clojure/clojure "1.7.0" :scope "provided"]
                  [org.clojure/clojurescript "1.7.170" :scope "provided"]
-                 [funcool/cats "1.2.0-SNAPSHOT"]]
+                 [funcool/cats "1.2.0"]]
   :deploy-repositories {"releases" :clojars
                         "snapshots" :clojars}
   :source-paths ["src" "assets"]
