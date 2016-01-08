66c8b484fafa1731cfda85a1edfca11f73b45757
Andrey Antukh <niwi@niwi.nz>
2016-01-08 19:55:25 +0200

    Add type annotations to externs file.

--- assets/_bluebird/externs.js
+++ assets/_bluebird/externs.js
@@ -1,20 +1,98 @@
-var setTimeout = function() {};
+/**
+ * @constructor
+ */
 var Promise = function() {};
 
+/**
+ * @this {Promise}
+ * @return {null}
+ */
 Promise.prototype.config = function() {};
+/**
+ * @this {Promise}
+ * @return {null}
+ */
 Promise.prototype.cancel = function() {};
+
+/**
+ * @this {Promise}
+ * @return {Promise}
+ */
 Promise.prototype.finally = function() {};
+/**
+ * @this {Promise}
+ * @return {Promise}
+ */
 Promise.prototype.then = function() {};
+
+/**
+ * @this {Promise}
+ * @return {Promise}
+ */
 Promise.prototype.catch = function() {};
+
+/**
+ * @this {Promise}
+ * @return {boolean}
+ */
 Promise.prototype.isRejected = function() {};
+
+/**
+ * @this {Promise}
+ * @return {boolean}
+ */
 Promise.prototype.isFulfilled = function() {};
+
+/**
+ * @this {Promise}
+ * @return {boolean}
+ */
 Promise.prototype.isPending = function() {};
+
+/**
+ * @this {Promise}
+ * @return {object}
+ */
 Promise.prototype.value = function() {};
+
+/**
+ * @this {Promise}
+ * @return {Error|object}
+ */
 Promise.prototype.reason = function() {};
+
+/**
+ * @this {Promise}
+ * @return {Promise}
+ */
 Promise.prototype.timeout = function() {};
 
+/**
+ * @this {null}
+ * @return {Promise}
+ */
 Promise.all = function() {};
+
+/**
+ * @this {null}
+ * @return {Promise}
+ */
 Promise.any = function() {};
+
+/**
+ * @this {null}
+ * @return {Promise}
+ */
 Promise.resolve = function() {};
+
+/**
+ * @this {null}
+ * @return {Promise}
+ */
 Promise.reject = function() {};
+
+/**
+ * @this {null}
+ * @return {Promise}
+ */
 Promise.delay = function() {};
