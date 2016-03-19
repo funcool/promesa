6a7a251105272a2de4cd54c23152b1f233e9de70
Andrey Antukh <niwi@niwi.nz>
2016-03-19 10:23:12 +0200

    Set version 1.1.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject funcool/promesa "1.1.0"
+(defproject funcool/promesa "1.1.1"
   :description "A promise library for ClojureScript"
   :url "https://github.com/funcool/promesa"
   :license {:name "BSD (2 Clause)"
