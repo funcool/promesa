68460fcd1b4d28571e379e51137f030dbe197071
Andrey Antukh <niwi@niwi.nz>
2016-06-08 17:59:25 +0300

    Update cljs compiler to 1.9.36

--- project.clj
+++ project.clj
@@ -4,8 +4,7 @@
   :license {:name "BSD (2 Clause)"
             :url "http://opensource.org/licenses/BSD-2-Clause"}
   :dependencies [[org.clojure/clojure "1.8.0" :scope "provided"]
-                 [org.clojure/clojurescript "1.8.51" :scope "provided"]
-                 [funcool/cats "1.2.1" :scope "provided"]]
+                 [org.clojure/clojurescript "1.9.36" :scope "provided"]]
   :deploy-repositories {"releases" :clojars
                         "snapshots" :clojars}
   :source-paths ["src" "assets"]
