c4a7885b686ccab03794d75832acc3500e9eb405
Andrey Antukh <niwi@niwi.nz>
2019-09-29 12:48:04 +0200

    Minor code changing on test/user ns.

--- test/user.clj
+++ test/user.clj
@@ -1,19 +1,31 @@
 (ns user
   (:require [clojure.tools.namespace.repl :as r]
-            [criterium.core :refer [quick-bench with-progress-reporting]]
+            [criterium.core :refer [quick-bench bench with-progress-reporting]]
             [clojure.walk :refer [macroexpand-all]]
             [clojure.pprint :refer [pprint]]
             [clojure.test :as test]
             [promesa.core :as p]
-            [promesa.protocols :as pt]))
+            [promesa.protocols :as pt]
+            [promesa.util :as pu])
+  (:import java.util.concurrent.CompletableFuture
+           java.util.concurrent.CompletionStage
+           java.util.function.Function))
 
-(defmacro bench
+(defmacro run-quick-bench
   [& exprs]
   `(with-progress-reporting (quick-bench (do ~@exprs) :verbose)))
 
-(defmacro bench'
+(defmacro run-quick-bench'
   [& exprs]
   `(quick-bench (do ~@exprs)))
+
+(defmacro run-bench
+  [& exprs]
+  `(with-progress-reporting (bench (do ~@exprs) :verbose)))
+
+(defmacro run-bench'
+  [& exprs]
+  `(bench (do ~@exprs)))
 
 (defn- run-test
   ([] (run-test #"^promesa.tests.*"))
@@ -37,18 +49,20 @@
       (System/exit fail)
       (System/exit 0))))
 
-;; (def normal-var
-;;   (delay 1))
-
-;; (def ^:dynamic *dynamic-var*
-;;   (delay 1))
-
-;; (defn bench-fn1
+;; (defn simple-promise-chain-5-raw
 ;;   []
-;;   (deref normal-var))
+;;   @(as-> (CompletableFuture/completedFuture 1) $
+;;      (p/then' $ inc)
+;;      (p/then' $ inc)
+;;      (p/then' $ inc)
+;;      (p/then' $ inc)
+;;      (p/then' $ inc)))
 
-;; (defn bench-fn2
+;; (defn simple-completable-chain-5-raw
 ;;   []
-;;   (deref *dynamic-var*))
-
-
+;;   @(as-> (CompletableFuture/completedFuture 1) $
+;;      (.thenApply ^CompletionStage $ ^Function (pu/->FunctionWrapper inc))
+;;      (.thenApply ^CompletionStage $ ^Function (pu/->FunctionWrapper inc))
+;;      (.thenApply ^CompletionStage $ ^Function (pu/->FunctionWrapper inc))
+;;      (.thenApply ^CompletionStage $ ^Function (pu/->FunctionWrapper inc))
+;;      (.thenApply ^CompletionStage $ ^Function (pu/->FunctionWrapper inc))))
