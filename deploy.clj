71988aa44dcef7bb4d56bd230d84dd9653a4d5ef
Andrey Antukh <niwi@niwi.nz>
2019-10-03 18:08:29 +0200

    More changes to the build/deploy deps & scripts.

--- deploy.clj
+++ deploy.clj
@@ -1,9 +1,23 @@
-(require '[clojure.java.shell :as shell]
-         '[clojure.main])
+(require '[clojure.java.shell :as shell])
+(require '[clojure.tools.deps.alpha.script.generate-manifest :as manifest]
+         '[clojure.data.xml :as xml])
+(require '[hf.depstar.uberjar :refer [uber-main]])
+
 
 (defmulti task first)
 
-(defmethod task :default
+(defmethod task "update:pom"
+  [args]
+  (alter-var-root #'xml/event-seq (fn [f]
+                                    (fn [source opts]
+                                      (f source (merge {:skip-whitespace true} opts)))))
+  (manifest/-main "--gen" "pom" "--config-files" "deps.edn"))
+
+(defmethod task "update:jar"
+  [args]
+  (uber-main {:dest "target/promesa.jar" :jar :thin} []))
+
+(defmethod task "deploy"
   [args]
   (let [result (shell/sh "mvn" "deploy:deploy-file"
                          "-Dfile=target/promesa.jar"
@@ -14,5 +28,12 @@
     (binding [*out* *err*]
       (println (:err result)))
     (System/exit (:exit result))))
+
+(defmethod task :default
+  [args]
+  (let [all-tasks  (-> task methods (dissoc :default) keys sort)
+        interposed (->> all-tasks (interpose ", ") (apply str))]
+    (println "Unknown or missing task. Choose one of:" interposed)
+    (System/exit 1)))
 
 (task *command-line-args*)
