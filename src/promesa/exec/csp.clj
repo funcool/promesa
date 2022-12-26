3d55a9725e732c8a222c252c194e97ea913af25a
Andrey Antukh <niwi@niwi.nz>
2022-12-26 11:58:20 +0100

    Fix channel constructor assets (regression introduced in prev commits)

--- src/promesa/exec/csp.clj
+++ src/promesa/exec/csp.clj
@@ -74,8 +74,10 @@
   ([buf] (chan buf nil nil))
   ([buf xf] (chan buf xf nil))
   ([buf xf exh]
-   (assert (or (nil? buf) (pos? buf))
-           "`buf` can be nil or positive int")
+   (assert (or (nil? buf)
+               (and (int? buf) (pos? buf))
+               (satisfies? pt/IBuffer buf))
+           "`buf` can be nil, positive int or IBuffer instance")
    (assert (or (nil? xf) (and (fn? xf) (some? buf)))
            "xf can be nil or fn (if fn, buf should be present")
    (let [buf (if (number? buf)
